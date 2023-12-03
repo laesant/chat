6667aad368e8d8d221ba163a7ea01cf4b65a3de0
Laênio Marques <105957017+laesant@users.noreply.github.com>
2023-12-03 13:28:30 -0300

    message bubble component

--- lib/components/message_bubble.dart
+++ lib/components/message_bubble.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:chat/core/models/chat_message.dart';
 import 'package:flutter/material.dart';
 
@@ -8,13 +10,38 @@ class MessageBubble extends StatelessWidget {
   const MessageBubble(
       {super.key, required this.message, required this.belongsToCurrentUser});
 
+  Widget _showUserImage(String imageUrl, BuildContext context) {
+    ImageProvider? provider;
+    if (imageUrl.startsWith('http')) {
+      provider = NetworkImage(imageUrl);
+    } else if (imageUrl.startsWith('file')) {
+      provider = FileImage(File(imageUrl));
+    } else {
+      provider = AssetImage(imageUrl);
+    }
+
+    return CircleAvatar(
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      radius: 15,
+      backgroundImage: provider,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: belongsToCurrentUser
           ? MainAxisAlignment.end
           : MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
+        if (!belongsToCurrentUser)
+          Row(
+            children: [
+              const SizedBox(width: 8),
+              _showUserImage(message.userImageUrl, context)
+            ],
+          ),
         Container(
             constraints: const BoxConstraints(maxWidth: 180),
             padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
@@ -23,7 +50,16 @@ class MessageBubble extends StatelessWidget {
                 color: belongsToCurrentUser
                     ? Theme.of(context).colorScheme.inversePrimary
                     : Theme.of(context).colorScheme.onPrimary,
-                borderRadius: BorderRadius.circular(12)),
+                borderRadius: BorderRadius.only(
+                  topLeft: belongsToCurrentUser
+                      ? const Radius.circular(12)
+                      : const Radius.circular(0),
+                  topRight: belongsToCurrentUser
+                      ? const Radius.circular(0)
+                      : const Radius.circular(12),
+                  bottomLeft: const Radius.circular(12),
+                  bottomRight: const Radius.circular(12),
+                )),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
@@ -38,6 +74,7 @@ class MessageBubble extends StatelessWidget {
                         decorationStyle: TextDecorationStyle.solid,
                         decorationThickness: 2),
                   ),
+                const SizedBox(height: 4),
                 Text(message.text),
               ],
             )),
