3653aeb093e149f8913222767a4c7d83ea0a9f29
Laênio Marques <105957017+laesant@users.noreply.github.com>
2023-12-19 12:10:16 -0300

    Push Notification #01

--- lib/core/services/notification/chat_notification_service.dart
+++ lib/core/services/notification/chat_notification_service.dart
@@ -1,8 +1,9 @@
 import 'package:chat/core/models/chat_notification.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
 class ChatNotificationService with ChangeNotifier {
-  List<ChatNotification> _items = [];
+  final List<ChatNotification> _items = [];
 
   List<ChatNotification> get items => [..._items];
   int get itemsCount => items.length;
@@ -15,5 +16,27 @@ class ChatNotificationService with ChangeNotifier {
   void remove(int index) {
     _items.removeAt(index);
     notifyListeners();
+  }
+
+  // Push Notification
+  Future<void> init() async {
+    await _configureForeground();
+  }
+
+  Future<bool> get _isAuthorized async {
+    final messaging = FirebaseMessaging.instance;
+    final NotificationSettings settings = await messaging.requestPermission();
+    return settings.authorizationStatus == AuthorizationStatus.authorized;
+  }
+
+  Future<void> _configureForeground() async {
+    if (await _isAuthorized) {
+      FirebaseMessaging.onMessage.listen((msg) {
+        if (msg.notification == null) return;
+        add(ChatNotification(
+            title: msg.notification!.title ?? "Não Informado!",
+            body: msg.notification!.body ?? "Não Informado!"));
+      });
+    }
   }
 }
